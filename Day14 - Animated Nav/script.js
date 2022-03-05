f249f6ee928eb226880c01a8c27b1ecbd1de85b2
Reid <reid@sionnachsoftware.com>
2022-03-05 09:46:45 -0500

    Day14 - Completed

---
+++ Day14 - Animated Nav/script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('toggle');
+const nav = document.getElementById('nav');
+
+btn.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+doc
+
