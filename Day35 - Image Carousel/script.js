00c9adeedbf443f2d270674cee6aa4f8c581dcab
jamie-reid <jamie.robert.reid@gmail.com>
2022-04-01 15:55:25 -0400

    Day 35 Completed

---
+++ Day35 - Image Carousel/script.js
@@ -0,0 +1,39 @@
+const imgs = document.getElementById('imgs');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+
+const img = document.querySelectorAll('#imgs img');
+
+let index = 0;
+let interval = setInterval(run, 4000);
+
+function run() {
+    index++
+    changeImage()
+}
+
+function changeImage() {
+    if (index > img.length - 1) {
+        index = 0
+    } else if (index < 0) {
+        index = img.length
+    }
+    imgs.style.transform = `translateX(-${index * 500}px)`
+}
+
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, 4000)
+}
+
+leftBtn.addEventListener('click', () => {
+    index--
+    changeImage()
+    resetInterval()
+})
+
+rightBtn.addEventListener('click', () => {
+    index++
+    changeImage()
+    resetInterval()
+})
